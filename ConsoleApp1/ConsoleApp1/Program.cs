c8558b6d113b46123ec32b1e321c89857cabb67e
Menna Omar <mennaomar2004@gmail.com>
2024-10-26 22:51:18 +0300

    my first projects

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name ");
+            string name = Console.ReadLine();   
+            Console.WriteLine("Hello "+name+ " !");
+            Console.WriteLine("Enter your age ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your age " + age +" .");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the first num");
+            string firstnum = Console.ReadLine();
+            Console.WriteLine("Enter the second num");
+            string secndnum = Console.ReadLine();
+            int result = int.Parse(firstnum) + int.Parse(secndnum);
+            Console.WriteLine("result = "+result);
+
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine(" enter your age");
+            int age =int.Parse( Console.ReadLine());
+            if (age <= 18) Console.WriteLine("child");
+            else if(age>18 & age<20)
+            {
+               Console.WriteLine("teenager");
+            }
+            else {
+                Console.WriteLine("adult");
+            }
+        }
+    }
+}
